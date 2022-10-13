f5ed07d30f101469fe5199a0848a79128ee231c8
Sopuru Okeke <sopuru72@gmail.com>
2022-10-13 08:34:32 -0700

    script 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *
+ * main - to use put to display a sentence
+ *
+ * Return - Return 0 if successful
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+/*to display Programming is like building a multilingual puzzle */
+	return (0);
+}
