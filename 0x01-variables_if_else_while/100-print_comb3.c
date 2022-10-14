4d28271e599fa248140edcbd187478921feee7e9
Sopuru Okeke <sopuru72@gmail.com>
2022-10-14 13:17:54 -0700

    100 a

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,22 +7,22 @@ int main(void)
 {
 	int a, b, c;
 
-	c = 0;
+	c = '0';
 
-	for (a = 0; a <= 9; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = '0'; b <= '9'; b++)
 		{
 			if (a != b)
 			{
 			putchar(a);
 			putchar(b);
 			}
-			else if (a == b)
+			if (a == b)
 			{
 				continue;
 			}
-			else if (a == 8 && b == 9)
+			if (a == '8' && b == '9')
 			{
 				break;
 			}
