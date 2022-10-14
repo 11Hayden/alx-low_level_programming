316b43e6b9e7e9580ccf49ccbb47f096c82fe8ab
Sopuru Okeke <sopuru72@gmail.com>
2022-10-14 09:07:58 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b, a;
+
+	for (a = 97; a < 123; a++)
+{
+	putchar(a);
+}
+	for (b = 65; b < 91; b++)
+{
+	putchar(b);
+}
+	putchar('\n');
+return (0);
+}
