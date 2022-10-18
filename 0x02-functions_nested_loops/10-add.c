6bbf17ad714af00c5b20ee5eb0453362660da4e2
Sopuru Okeke <sopuru72@gmail.com>
2022-10-18 12:49:16 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add - to add two integers together
+*@a: number one.
+*@b: number two.
+* Return: to return the result
+*/
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
