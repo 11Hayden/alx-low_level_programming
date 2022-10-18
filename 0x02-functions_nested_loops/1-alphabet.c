9432a308f7de28764a05a5c0050397fb9b9c7beb
Sopuru Okeke <sopuru72@gmail.com>
2022-10-18 06:39:23 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* main - to print alphabets with main.h as header
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int a;
+	for (a = 97; a <= 122; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+return (0);
+}
