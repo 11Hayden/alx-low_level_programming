0416f3a332d0cd1bf29cf371d017c75d9d86158f
Sopuru Okeke <sopuru72@gmail.com>
2022-10-18 05:32:49 -0700

    0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c)
+
+#endif
+/**
+ *File name : main.h
+ *Description : header filr for all the functions required
+ * Author : Sopuru Okeke
+ */
+
+void _putchar(char c);
