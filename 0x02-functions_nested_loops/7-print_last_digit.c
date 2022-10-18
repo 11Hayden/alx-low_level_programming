4c257b7af967c1a0367b59ec3580f291738c362e
Sopuru Okeke <sopuru72@gmail.com>
2022-10-18 10:29:41 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_last - to print the last digit of a given input
+* @n:  is the variable
+* Return: n if n>=0 else -n
+*/
+int print_last_digit(int n)
+{
+	int m;
+
+	m = n % 10;
+	if (m < 0)
+	{
+		m = m * -1;
+	}
+	_putchar(m + '0');
+	return (m);
+}
