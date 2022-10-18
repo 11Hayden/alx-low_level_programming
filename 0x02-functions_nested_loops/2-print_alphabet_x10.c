de3dd5286592745a6f56f47bce2e617411a69fea
Sopuru Okeke <sopuru72@gmail.com>
2022-10-18 07:36:29 -0700

    2 a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,13 @@
 */
 void print_alphabet_x10(void)
 {
-int a;
-	for (a = 97; a <= 122; a++)
+int a, b;
+	for (a = 0; a < 10; a++)
 	{
-		_putchar(a);
-		for (a = 0; a < 10; a++)
+		_putchar('\n');
+		for (b = 97; b <= 122; b++)
 		{
-			_putchar('\n');
+			_putchar(b);
 		}
 	}
 }
