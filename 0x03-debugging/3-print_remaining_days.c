884c76792267f80450eff03630688612ec2e35bb
Sopuru Okeke <sopuru72@gmail.com>
2022-10-19 15:56:12 -0700

    300

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -23,6 +23,13 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
+	if (month != 2 && day != 60 && year % 4 == 0)
+	{
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
+	}
+	else
+	{
 	if (month == 2 && day == 60 && year % 4 == 0)
 	{
 	printf("Day of the year: %d\n", day);
@@ -38,6 +45,7 @@ void print_remaining_days(int month, int day, int year)
 	{
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 365 - day);
+	}
 	}
 	}
 	}
