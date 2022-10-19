0da494edc5419f2ec50edc08d28ab69181cd0c65
Sopuru Okeke <sopuru72@gmail.com>
2022-10-19 14:50:20 -0700

    10:49

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -13,15 +13,14 @@ int main(void)
 	int day;
 	int year;
 
-	month = 2;
-	day = 29;
-	year = 2427;
+	month = 4;
+	day = 1;
+	year = 2000;
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
 
 	day = convert_day(month, day);
 
 	print_remaining_days(month, day, year);
-
-	return (0);
+return (0);
 }
