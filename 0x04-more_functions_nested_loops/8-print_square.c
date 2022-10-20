811923567b8e5d6cabd3a2a36ad024726460046b
Sopuru Okeke <sopuru72@gmail.com>
2022-10-20 13:35:14 -0700

    8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,15 +8,14 @@ void print_square(int size)
 {
 	int m, l;
 
-	if (size > 0)
+	for (m = 0; m < size; m++)
 	{
-		for (m = 0; m < size; m++)
+		for (l = 0; l < size; l++)
 		{
-			for (l = 0; l < size; l++)
-			{
 			_putchar('#');
-			}
-			_putchar('\n');
 		}
+		if (m != size - 1)
+		_putchar('\n');
 	}
+	_putchar('\n');
 }
