d2aea505c369b683a896e1503fd66949e7a8986d
Sopuru Okeke <sopuru72@gmail.com>
2022-10-20 07:02:35 -0700

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _isdigit - to check for digit
+* @c: integer c
+* Return: returns 1 if uppercase
+*/
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
