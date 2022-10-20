09569e66f4a55d8c030b04d77a1f28d9bfb7d004
Sopuru Okeke <sopuru72@gmail.com>
2022-10-20 11:16:34 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,13 @@ void more_numbers(void)
 {
 	int a, b;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b <= 14; b++)
 		{
-			_putchar(b);
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
