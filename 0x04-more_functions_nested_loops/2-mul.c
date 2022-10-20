120d1ab028986b58ffe620d2007a276ab741dac0
Sopuru Okeke <sopuru72@gmail.com>
2022-10-20 07:18:48 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* mul - to multiply 2 digits
+* @a: first digit
+* @b: second digit
+* Return: 0 always
+*/
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
