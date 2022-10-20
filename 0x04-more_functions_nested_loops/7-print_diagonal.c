909739457175da0a9a2dd6273c0e4748a92b8b4c
Sopuru Okeke <sopuru72@gmail.com>
2022-10-20 12:50:55 -0700

    7ish

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,12 +10,12 @@ int m, k;
 
 		for (m = 0; m < n; m++)
 		{
-			for ( k = 0; k <= m; k++)
+			for (k = 0; k <= m; k++)
 			{
 				_putchar(' ');
 			}
 			_putchar('\\');
-			if (m < (n-1))
+			if (m < (n - 1))
 				_putchar('\n');
 		}
 		_putchar('\n');
