2bb29fbef2650b915c7b28738ecf03cb9d17498f
Sopuru Okeke <sopuru72@gmail.com>
2022-10-20 08:01:17 -0700

    5" 5" "
    "g"
    git "55"
    
    :
    q
    "nn"
    "

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* more_numbers - to print 0-14 10 times
+* Return: 0 always
+*/
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '14'; b++)
+		{
+			_putchar(b);
+		}
+	}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
