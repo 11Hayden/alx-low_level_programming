1f969335632dc37e90dd062d4c872e3ada707d36
Sopuru Okeke <sopuru72@gmail.com>
2022-10-24 12:39:39 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* rev_string - to print the reverse of a string
+* @s: character
+*/
+
+void rev_string(char *s)
+{
+	int len = 0;
+	int i, j;
+	char temp, *st;
+
+	while (s[len] != '\0')
+		len++;
+	st = s;
+	for (i = 0; i < (len - 1); i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(st + j);
+			(st + j) = (st + (j - 1));
+			*(st + (j - 1)) = temp;
+		}
+	}
+}
