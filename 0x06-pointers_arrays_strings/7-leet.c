6ee57ab3dae2fbee315f8641ccef61b39662accf
Sopuru Okeke <sopuru72@gmail.com>
2022-10-27 06:20:07 -0700

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* leet - to encode a string to 1337
+* @s: pointer
+* Return: s
+*/
+
+char *leet(char *s)
+{
+	int n, m;
+	char s1[] = "aaeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (n = 0; s[n] != '\0'; n++)
+	{
+		for (m = 0; m <= 9; m++)
+		{
+			if (s[n] == s1[m])
+				s[n] = s2[m];
+		}
+	}
+return (s);
+}
