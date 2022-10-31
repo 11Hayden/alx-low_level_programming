aaeae119a1abda2e179d206cc157abfe36eeb530
Sopuru Okeke <sopuru72@gmail.com>
2022-10-31 06:15:48 -0700

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _strchr - to locate a character in a string
+* @s: character
+* @c: another character
+* Return: returns a pointer to s
+*/
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; s++)
+		if (*s == c)
+			return ((char *)s);
+return ("NULL");
+}
