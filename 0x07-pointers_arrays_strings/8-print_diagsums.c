b10d09ff5a4eb3e6e64869c26cafe67e47792cda
Sopuru Okeke <sopuru72@gmail.com>
2022-10-31 08:40:31 -0700

    7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_diagsums - to print the sum of the two diagonals of a square matrix
+* @a : integer
+*/
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int total1 = 0, total2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		total1 += a[i];
+		a += size;
+	}
+	a -= size;
+	for (i = 0; i < size; i++)
+	{
+		total2 += a[i];
+		a -= size;
+	}
+	printf("%d, %d\n", total1, total2);
+}
