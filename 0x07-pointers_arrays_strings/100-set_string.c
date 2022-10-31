7cd5513aa8848ed41832e15d3562a54c538fbf1d
Sopuru Okeke <sopuru72@gmail.com>
2022-10-31 08:55:38 -0700

    100 & 101

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* set_string - sets value of a character to pointer
+* @s: pointer
+* @to: a character
+* Return: returns a pointer to s
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
