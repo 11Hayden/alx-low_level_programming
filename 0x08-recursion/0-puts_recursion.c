e5a34733e1bbb074e3b0607ed7c5832929e6378f
Sopuru Okeke <sopuru72@gmail.com>
2022-11-02 05:27:56 -0700

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _puts_recursion - to print out a string followed by a new line
+* @s: pointer s
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		break;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
