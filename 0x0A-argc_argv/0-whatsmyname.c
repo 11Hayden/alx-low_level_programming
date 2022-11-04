79563ebfea84fede5c547f5eb6b6a64aacf53135
Sopuru Okeke <sopuru72@gmail.com>
2022-11-04 05:08:24 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - entry point
+* @argc: integer
+* @argv: pointer
+* Return: can be 0
+*/
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[0]);
+return (argc);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+this is a REAADME for 0x0A-argc_argv
