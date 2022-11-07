ae69c824ae6b2e12ce7e2c4421729bdd8db7909e
Sopuru Okeke <sopuru72@gmail.com>
2022-11-07 11:49:44 -0800

    121

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,59 +1,27 @@
 #include "main.h"
 /**
-* _strlen - to know lenght of a string
-* @s: character
-* Return: return len
-*/
-int _strlen(char *s)
-{
-	if (*s == '\0')
-	{
-		return (0);
-	}
-	else
-	{
-		return (1 + _strlen(++s));
-	}
-}
-
-/**
-* _strcpy - copies from memory area src to memory area dest
-* @dest: destination string
-* @src: source string
-* Return: returns a pointer to s
-*/
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-return (dest);
-}
-
-/**
 * _strdup - a function that returns a pointer
 * @str: pointer
 * Return: pointer
 */
 char *_strdup(char *str)
 {
-char *sa;
-unsigned int size = _strlen(str) + 1;
+	char *sa;
+	unsigned int size, i;
+
+	i = 0;
+	size = 0;
 
 	if (str == 0)
 	{
 		return (NULL);
 	}
-	sa = (char *) malloc(size * sizeof(char));
-if (sa == NULL)
-{
-	return (NULL);
-}
-_strcpy(sa, str);
+	sa = malloc((size + 1) * sizeof(char));
+	if (sa == NULL)
+	{
+		return (NULL);
+	}
+	while ((sa[i] = str[i]) != '\0')
+		i++;
 return (sa);
 }
