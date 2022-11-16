03c230525621b98c9f8d1e48a7e23b86b63f7272
Sopuru Okeke <sopuru72@gmail.com>
2022-11-16 07:06:12 -0800

    0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+* print_name - to print a name
+* @name:the name
+* @f: function pointer
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
