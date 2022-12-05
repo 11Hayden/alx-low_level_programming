b4e4c50feaa619813adc3166d795659fd257e120
Hayden <sopuru72@gmail.com>
2022-12-05 10:29:28 -0800

    3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,72 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* ferror - a function that checks if a file can be opened.
+* @file_from: source file
+* @file_to: destination file
+* @argv: argument vector
+*/
+void feerror(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf (STDERR_FILENO, "Error: cant't read from file %s\n",
+				argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf (STDERR_FILENO, "Error: cant't read from file %s\n",
+				argv[2]);
+		exit(99);
+	}
+}
+
+/**
+* main - entry point
+* @argc: argument count
+* @argv: argument vector
+* Return: 0 always
+*/
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, err_close;
+	ssize_t charsno, wr;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf (STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	feerror(file_from, file_to, argv);
+	charsno = 1024;
+	while (charsno == 1024)
+	{
+		charsno = read(file_from, buf, 1024);
+		if (charsno == -1)
+			feerror(-1, 0, argv);
+		wr = write(file_to, buf, charsno);
+		if (wr == -1)
+			feerror(0, -1, argv);
+	}
+
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
+				file_from);
+		exit(100);
+	}
+
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
+				file_from);
+		exit(100);
+	}
+	return (0);
+}
